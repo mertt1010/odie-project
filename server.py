82affb19f745a400b97e0260478dd6cbf354ccb1
mertt1010 <132399949+mertt1010@users.noreply.github.com>
2025-04-27 14:26:24 +0300

    apiOK
    Api create delete disable enable OK.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from user_ops import add_user, disable_user, delete_user, get_ldap_connection
 from db_ops import get_db_connection
+import psycopg2
 
 app = FastAPI()
 
@@ -12,6 +13,12 @@ class UserCreateRequest(BaseModel):
     password: str
     role_id: int
     department_id: int | None = None
+
+class UserDisableRequest(BaseModel):
+    username: str
+
+class UserDeleteRequest(BaseModel):
+    username: str
 
 @app.post("/add_user")
 def api_add_user(user: UserCreateRequest):
@@ -28,10 +35,6 @@ def api_add_user(user: UserCreateRequest):
     conn_ldap.unbind()
     return {"success": success, "status": status}
 
-
-class UserDisableRequest(BaseModel):
-    username: str
-
 @app.post("/disable_user")
 def api_disable_user(user: UserDisableRequest):
     conn_ldap = get_ldap_connection()
@@ -39,3 +42,16 @@ def api_disable_user(user: UserDisableRequest):
     conn_ldap.unbind()
     return {"success": success, "status": status_text}
 
+@app.post("/enable_user")
+def api_enable_user(user: UserDisableRequest):
+    conn_ldap = get_ldap_connection()
+    success, status_text = disable_user(conn_ldap, user.username, enable=True)
+    conn_ldap.unbind()
+    return {"success": success, "status": status_text}
+
+@app.delete("/delete_user")
+def api_delete_user(user: UserDeleteRequest):
+    conn_ldap = get_ldap_connection()
+    success = delete_user(conn_ldap, user.username)
+    conn_ldap.unbind()
+    return {"success": success}
