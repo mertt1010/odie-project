8c910f4d0b50d39638c7fbe68134468b4c355db7
mertt1010 <132399949+mertt1010@users.noreply.github.com>
2025-05-21 14:44:21 +0300

    Update db_ops.py

--- backend/db_ops.py
+++ backend/db_ops.py
@@ -2,11 +2,12 @@ import psycopg2
 import os
 
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "gjM1ePvLQlLLlYZ4"
+DB_HOST = "db.zdgklkzmqwkkczhqishq.supabase.co"
+DB_PORT = "5432"
+
 
 def get_db_connection():
     try:
